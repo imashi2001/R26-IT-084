b375eb835b46ddcde5482e85336c6d55b4b55e53
adheeshana herath <adheeshanaherath07@gmail.com>
2026-09-01 12:51:53 +0530

    feat: add AdminPage for bin management and implement waste forecasting modules

--- web/components/livemonitoring/LiveBinMap.js
+++ web/components/livemonitoring/LiveBinMap.js
@@ -30,7 +30,7 @@ import { markerFillFromBin, fillLabel } from "../../utils/fillTier";
  */
 
 const TILE_URL =
-  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png";
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png";
 const TILE_ATTR =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>';
 
